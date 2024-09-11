10d778e7670e51cba07453396b7aa6313a695fe6
MD.SHOHAG RANA <143956320+shohagflow@users.noreply.github.com>
2024-09-11 11:08:44 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,30 @@
+
+sum(7,7);
+function sum(n1,n2){
+    console.log(n1+n2);
+}
+sum(5,6);
+
+function mat(b1,b2){
+    console.log(b1-b2);
+}
+
+mat(20,11);
+
+function mat2(b1,b2){
+    console.log(b1*b2);
+}
+
+mat2(20,3);
+
+function div(b1,b2){
+    console.log(b1/b2);
+}
+
+div(20,2);
+
+
+
+
+
+
